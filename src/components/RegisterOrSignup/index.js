cac5ce3a36dfce189ab3965cb1e4f504be0cf679
Guilherme Cabral <guicabralprowork@gmail.com>
2023-06-07 20:40:12 -0300

    feat:e estruturando tela de registro e login

---
+++ src/components/RegisterOrSignup/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+export default function RegisterOrSignup() {
+    return (
+        <View>
+            <Text>Oi</Text>
+        </View>
+    );
+}
